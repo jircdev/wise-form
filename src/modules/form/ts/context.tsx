60e20dbb8125823d3a1eb335e3f31f15c505f4d0
Julio Rodriguez <jircdev@gmail.com>
2024-02-20 10:07:00 -0300

    feat: Add wrappedFormContext

--- src/modules/form/ts/context.tsx
+++ src/modules/form/ts/context.tsx
@@ -11,6 +11,13 @@ export interface IFormContext {
 	};
 	formTypes?: Record<string, React.ElementType>;
 }
+
+export /*bundle */ interface IWrappedFormContext extends IFormContext {
+	parent: IFormContext;
+}
 const value: IFormContext = {};
 export const WiseFormContext = React.createContext(value);
 export /*bundle*/ const useWiseFormContext = () => React.useContext(WiseFormContext);
+
+export const WrappedWiseFormContext = React.createContext(value);
+export /*bundle*/ const useWrappedWiseFormContext = () => React.useContext(WrappedWiseFormContext);
